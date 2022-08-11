ba33700dbc0aad0a24618b79fa9e47df7733dd52
Zakaria09 <92021986+Zaax09@users.noreply.github.com>
2022-08-11 16:56:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const num1 = Math.ceil(Math.random()*1000);
+const num2 = Math.ceil(Math.random()*1000);
+
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const questionEl = document.getElementById("question");
+const scoreEl = document.getElementById("score");
+
+
+
+let score = JSON.parse (localStorage.getItem("score"));
+if(!score){
+    score = 0;
+}
+
+scoreEl.innerHTML = `score: ${score}`
+if(score >= 0){
+    scoreEl.style.color = "green";
+}if (score < 0) {
+    scoreEl.style.color = "red";
+    
+} 
+
+
+
+questionEl.innerText =`What is ${num1} multiply by  ${num2} ?`;
+
+const correctAns = num1 * num2;
+
+
+
+formEl.addEventListener("submit", ()=>{
+    const userAns = +inputEl.value;
+    if(userAns === correctAns) {
+        score++;
+        updateLocalStorage();
+    }else{
+        score--;
+        updateLocalStorage();
+    }
+});
+
+
+    function updateLocalStorage(){
+        localStorage.setItem("score", JSON.stringify(score));
+    }
